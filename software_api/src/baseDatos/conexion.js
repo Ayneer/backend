7fcf34dfd84f18b0729008873357fdb4139711df
Karen Benedetti <kbenedetti9@gmail.com>
2019-10-13 17:38:08 -0500

    Desplegando a heroku

--- software_api/src/baseDatos/conexion.js
+++ software_api/src/baseDatos/conexion.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb://localhost:27017/energia_db')
+mongoose.connect('mongodb://kbenedetti9:kbenedetti9@ds333248.mlab.com:33248/heroku_560nxjg3')
 .then(console.log('Conectando a la base de datos...'))
 .catch(err => console.log("Error al intentar conectar con MongoDB."));
