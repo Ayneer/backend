03dbc3a08845b868e49308b0916070b985036323
Ayneer Gonzalez <ayneer12@gmail.com>
2019-10-21 20:54:28 -0500

    despliegue v2

--- software_api/src/baseDatos/conexion.js
+++ software_api/src/baseDatos/conexion.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-// mongoose.connect('mongodb://kbenedetti9:kbenedetti9@ds333248.mlab.com:33248/heroku_560nxjg3')
-mongoose.connect('mongodb://localhost:27017/energia_db')
+mongoose.connect('mongodb://kbenedetti9:kbenedetti9@ds333248.mlab.com:33248/heroku_560nxjg3')
+// mongoose.connect('mongodb://localhost:27017/energia_db')
 .then(console.log('Conectando a la base de datos...'))
 .catch(err => console.log(err));
