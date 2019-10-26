6948500a95e0fa3a648a26a11db5551de0bd9125
Karen Benedetti <kbenedetti9@gmail.com>
2019-10-26 16:52:32 -0500

    Cerrando sesion 3

--- software_api/Api/src/Modelos/Sistema.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const esquema_Sistema = new mongoose.Schema({
-    costoUnitario: { type: Number, required: true}
-});
-
-module.exports = mongoose.model('sistemas', esquema_Sistema);
--- software_api/src/Rutas/rutas.js
+++ software_api/src/Rutas/rutas.js
@@ -234,7 +234,7 @@ rutas.put('/sistema', async (req, res) => {
 rutas.get('sistema', async (req, res) => {
     if (cAutenticacion.estoyAutenticado(req) && await cAdministrador.buscarAdministradorCorreo(req.user.correo)) {//si esta auntenticado un administrador
         const sistema = await cAdministrador.obtenerDatosSistema();
-        res.status(400).send({ error: false, estado: true, mensaje: "Configuracion del sistema obtenida con exito", sistema });
+        res.status(400).send({ error: false, estado: true, mensaje: "Configuracion del sistema obtenida con exito", sistema: sistema });
     }else{
         res.status(401).send({ error: false, estado: false, mensaje: "No estas autenticado, debes iniciar sesion." });
     }
